78236aa7769fb85350072be21ca56a80dd7ba034
“david” <david@signority.com>
2024-04-05 23:04:33 -0700

    modify form

--- swing/src/main/java/org/elac/form/RegisterForm.java
+++ swing/src/main/java/org/elac/form/RegisterForm.java
@@ -1,7 +1,9 @@
 package org.elac.form;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -13,14 +15,16 @@ import javax.swing.JTextField;
  */
 public class RegisterForm extends JFrame {
 
-    private JTextField firstName;
     private JPanel jPanel;
+    private JButton submitButton;
+    private JTextField textField1;
+    private JTextField textField2;
 
     public RegisterForm() {
         setTitle("conscious-program GUI");
         setContentPane(jPanel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800, 1024);
+        setSize(480, 600);
         setLocationRelativeTo(null);
         setVisible(true);
     }
